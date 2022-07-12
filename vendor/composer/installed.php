0ee17d3793ef03b463e4cc8c2f6575441eec39a2
Pea <pea@misfist.com>
2022-07-12 17:58:59 -0400

    Add autoload files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '359fc506d47a252ba8a8780b0284ac45307dadcc',
+        'name' => 'misfist/site-functionality',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'misfist/site-functionality' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '359fc506d47a252ba8a8780b0284ac45307dadcc',
+            'dev_requirement' => false,
+        ),
+    ),
+);
