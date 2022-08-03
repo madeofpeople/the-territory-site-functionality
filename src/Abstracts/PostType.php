01c6574e447e527c9cfeae75611e85ceafcc7a90
Pea <pea@misfist.com>
2022-08-03 18:08:42 -0400

    Assign template param

--- src/Abstracts/PostType.php
+++ src/Abstracts/PostType.php
@@ -22,11 +22,6 @@ abstract class PostType extends Base {
 	public const POST_TYPE = self::POST_TYPE;
 
 	/**
-	 * PostType template
-	 */
-	public const TEMPLATE = self::TEMPLATE;
-
-	/**
 	 * Post Type fields
 	 */
 	public const FIELDS = self::FIELDS;
@@ -117,11 +112,10 @@ abstract class PostType extends Base {
 			'show_in_rest'        => array_key_exists( 'show_in_rest', $this::POST_TYPE ) ? $this::POST_TYPE['show_in_rest'] : true,
 			'rest_base'           => array_key_exists( 'rest_base', $this::POST_TYPE ) ? $this::POST_TYPE['rest_base'] : $this::POST_TYPE['archive'],
 		);
-		
-		if( !empty( $this::TEMPLATE ) ) {
-			$args['template'] = $this::TEMPLATE;
+		if( array_key_exists( 'template', $this::POST_TYPE ) && ! empty( $this::POST_TYPE['template'] ) ) {
+			$args['template'] = $this::POST_TYPE['template'];
 		}
-		
+
 		\register_post_type(
 			$this::POST_TYPE['id'],
 			\apply_filters( \get_class( $this ) . '\Args', $args )
