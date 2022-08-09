b63f743df96cc27f455684353df91d24f1c293de
Pea <pea@misfist.com>
2022-08-09 10:41:38 -0400

    Code formatting
    No code changes

--- blocks/src/social-cards/index.php
+++ blocks/src/social-cards/index.php
@@ -42,45 +42,45 @@ function render( $attributes, $content, $block ) {
 		while ( $query->have_posts() ) :
 			$query->the_post();
 
-			$post_id  = $query->post->ID;
+			$post_id        = $query->post->ID;
 			$share_services = array(
 				'twitter',
 				'facebook',
 			);
-			$services = array(
+			$services       = array(
 				'instagram',
-				'download'
+				'download',
 			);
 
-			$images = \get_post_meta( $post_id, 'images', true );
-			$extra_classname = $images && count( $images ) > 1  ? ' has-multiple-images' : '';
+			$images          = \get_post_meta( $post_id, 'images', true );
+			$extra_classname = $images && count( $images ) > 1 ? ' has-multiple-images' : '';
 
-			if( !empty( $images ) ) :
+			if ( ! empty( $images ) ) :
 
 				$output .= '<article id="post-' . $post_id . '" class="social-post' . $extra_classname . '">';
 
-					if ( $images ) {
-						$output .= '<ul class="image-group">';
-						foreach ( $images as $image_id ) {
-							$output .= sprintf( '<li id="image-%s">%s</li>', (int) $image_id, \wp_get_attachment_image( $image_id, 'full' ) );
-						}
-						$output .= '</ul><!--.image-group-->';
+				if ( $images ) {
+					$output .= '<ul class="image-group">';
+					foreach ( $images as $image_id ) {
+						$output .= sprintf( '<li id="image-%s">%s</li>', (int) $image_id, \wp_get_attachment_image( $image_id, 'full' ) );
 					}
+					$output .= '</ul><!--.image-group-->';
+				}
 
 					$output .= '<div class="share-actions">';
 
 						$output .= '<ul class="wp-block-outermost-social-sharing is-style-logos-only">';
 
-						if( \get_post_meta( $post_id, 'link', true ) || \get_post_meta( $post_id, 'message', true ) ) {
-							foreach ( $share_services as $service ) :
-								$output .= render_block_social_sharing_link( $service, $post_id );
-							endforeach;
-						}
+				if ( \get_post_meta( $post_id, 'link', true ) || \get_post_meta( $post_id, 'message', true ) ) {
+					foreach ( $share_services as $service ) :
+						$output .= render_block_social_sharing_link( $service, $post_id );
+					endforeach;
+				}
 
-						foreach ( $services as $service ) :
-							if( \get_post_meta( $post_id, $service, true ) ) {
-								$output .= render_block_social_sharing_link( $service, $post_id );
-							}
+				foreach ( $services as $service ) :
+					if ( \get_post_meta( $post_id, $service, true ) ) {
+						$output .= render_block_social_sharing_link( $service, $post_id );
+					}
 						endforeach;
 
 						$output .= '</ul><!--.wp-block-social-links-->';
@@ -122,7 +122,7 @@ function enqueue() {
 	$handle = 'slick';
 	if ( is_singular() ) {
 		$post_id = get_the_ID();
-		if( ! \wp_script_is( $handle ) ) {
+		if ( ! \wp_script_is( $handle ) ) {
 			\wp_register_script(
 				$handle,
 				SITE_CORE_DIR_URI . 'blocks/src/lib/slick-carousel/slick/slick.min.js',
@@ -131,7 +131,7 @@ function enqueue() {
 				true
 			);
 		}
-		if( ! \wp_style_is( $handle ) ) {
+		if ( ! \wp_style_is( $handle ) ) {
 			\wp_register_style(
 				$handle,
 				SITE_CORE_DIR_URI . 'blocks/src/lib/slick-carousel/slick/slick.css',
@@ -139,7 +139,7 @@ function enqueue() {
 				null
 			);
 		}
-		if( ! \wp_style_is( $handle . '-theme' ) ) {
+		if ( ! \wp_style_is( $handle . '-theme' ) ) {
 			\wp_register_style(
 				$handle . '-theme',
 				SITE_CORE_DIR_URI . 'blocks/src/lib/slick-carousel/slick/slick-theme.css',
@@ -266,11 +266,11 @@ function social_sharing_link_services( $service = '', $post_id = null ) {
 		$thumbnail = null;
 	}
 
-	$permalink = rawurlencode( \get_post_meta( $post_id, 'link', true ) );
+	$permalink  = rawurlencode( \get_post_meta( $post_id, 'link', true ) );
 	$card_title = \get_the_title( $post_id );
-	$title     = rawurlencode( \get_post_meta( $post_id, 'message', true ) );
-	$image     = rawurlencode( \esc_url( $thumbnail ) );
-	$separator = '%20&mdash;%20';
+	$title      = rawurlencode( \get_post_meta( $post_id, 'message', true ) );
+	$image      = rawurlencode( \esc_url( $thumbnail ) );
+	$separator  = '%20&mdash;%20';
 
 	$services_data = array(
 		'facebook'  => array(
@@ -348,9 +348,9 @@ function social_sharing_link_services( $service = '', $post_id = null ) {
 			'url'   => \get_post_meta( $post_id, 'instagram', true ),
 			'icon'  => '<svg width="24" height="24" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path d="M12,4.622c2.403,0,2.688,0.009,3.637,0.052c0.877,0.04,1.354,0.187,1.671,0.31c0.42,0.163,0.72,0.358,1.035,0.673 c0.315,0.315,0.51,0.615,0.673,1.035c0.123,0.317,0.27,0.794,0.31,1.671c0.043,0.949,0.052,1.234,0.052,3.637 s-0.009,2.688-0.052,3.637c-0.04,0.877-0.187,1.354-0.31,1.671c-0.163,0.42-0.358,0.72-0.673,1.035 c-0.315,0.315-0.615,0.51-1.035,0.673c-0.317,0.123-0.794,0.27-1.671,0.31c-0.949,0.043-1.233,0.052-3.637,0.052 s-2.688-0.009-3.637-0.052c-0.877-0.04-1.354-0.187-1.671-0.31c-0.42-0.163-0.72-0.358-1.035-0.673 c-0.315-0.315-0.51-0.615-0.673-1.035c-0.123-0.317-0.27-0.794-0.31-1.671C4.631,14.688,4.622,14.403,4.622,12 s0.009-2.688,0.052-3.637c0.04-0.877,0.187-1.354,0.31-1.671c0.163-0.42,0.358-0.72,0.673-1.035 c0.315-0.315,0.615-0.51,1.035-0.673c0.317-0.123,0.794-0.27,1.671-0.31C9.312,4.631,9.597,4.622,12,4.622 M12,3 C9.556,3,9.249,3.01,8.289,3.054C7.331,3.098,6.677,3.25,6.105,3.472C5.513,3.702,5.011,4.01,4.511,4.511 c-0.5,0.5-0.808,1.002-1.038,1.594C3.25,6.677,3.098,7.331,3.054,8.289C3.01,9.249,3,9.556,3,12c0,2.444,0.01,2.751,0.054,3.711 c0.044,0.958,0.196,1.612,0.418,2.185c0.23,0.592,0.538,1.094,1.038,1.594c0.5,0.5,1.002,0.808,1.594,1.038 c0.572,0.222,1.227,0.375,2.185,0.418C9.249,20.99,9.556,21,12,21s2.751-0.01,3.711-0.054c0.958-0.044,1.612-0.196,2.185-0.418 c0.592-0.23,1.094-0.538,1.594-1.038c0.5-0.5,0.808-1.002,1.038-1.594c0.222-0.572,0.375-1.227,0.418-2.185 C20.99,14.751,21,14.444,21,12s-0.01-2.751-0.054-3.711c-0.044-0.958-0.196-1.612-0.418-2.185c-0.23-0.592-0.538-1.094-1.038-1.594 c-0.5-0.5-1.002-0.808-1.594-1.038c-0.572-0.222-1.227-0.375-2.185-0.418C14.751,3.01,14.444,3,12,3L12,3z M12,7.378 c-2.552,0-4.622,2.069-4.622,4.622S9.448,16.622,12,16.622s4.622-2.069,4.622-4.622S14.552,7.378,12,7.378z M12,15 c-1.657,0-3-1.343-3-3s1.343-3,3-3s3,1.343,3,3S13.657,15,12,15z M16.804,6.116c-0.596,0-1.08,0.484-1.08,1.08 s0.484,1.08,1.08,1.08c0.596,0,1.08-0.484,1.08-1.08S17.401,6.116,16.804,6.116z"></path></svg>',
 		),
-		'download' => array(
+		'download'  => array(
 			'label' => __( 'Download ' . $card_title . ' images as a File', 'site-functionality' ),
-			'url'   => \wp_get_attachment_url(  \get_post_meta( $post_id, 'download', true ) ),
+			'url'   => \wp_get_attachment_url( \get_post_meta( $post_id, 'download', true ) ),
 			'icon'  => '<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path style="fill:none" d="M3.202 2.604h41.747v39.559H3.202z" transform="matrix(.57489 0 0 .6067 -1.841 -1.58)"/><path d="M11 40c-.8 0-1.5-.3-2.1-.9-.6-.6-.9-1.3-.9-2.1v-7.15h3V37h26v-7.15h3V37c0 .8-.3 1.5-.9 2.1-.6.6-1.3.9-2.1.9H11Zm13-7.65-9.65-9.65 2.15-2.15 6 6V8h3v18.55l6-6 2.15 2.15L24 32.35Z" style="fill-rule:nonzero" transform="matrix(.625 0 0 .625 -3 -3)"/></svg>',
 		),
 		'whatsapp'  => array(
